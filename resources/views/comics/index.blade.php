9f39f4ced2254624d0599a440788deb4e812aac9
Paolo Giovannini <giovannini13@gmail.com>
2023-05-17 14:38:29 +0200

    esercizio base concluso

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -12,8 +12,13 @@
                         <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $comic->title }}</h5>
-                            <a href="{{route('comics.show', ['comic' => $comic->id])}}" class="btn btn-primary">Dettagli fumetto</a>
-                            <a href="{{route('comics.edit', ['comic' => $comic->id])}}" class="btn btn-warning">Modifica fumetto</a>
+                            <a href="{{route('comics.show', ['comic' => $comic->id])}}" class="btn btn-primary mt-3">Dettagli fumetto</a>
+                            <a href="{{route('comics.edit', ['comic' => $comic->id])}}" class="btn btn-warning mt-3">Modifica fumetto</a>
+                            <form class="mt-3" action="{{route('comics.destroy', ['comic' => $comic->id])}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Elimina fumetto</button>
+                            </form>
                         </div>
                     </div>
                 </div>
